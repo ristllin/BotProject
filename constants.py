639197eccbe8f2b8f9d1c655418e35d5b73bbcf2
unknown <ristlin@gmail.com>
2019-07-11 01:00:50 +0300

    stable for benny, improved repetative enhancment

--- constants.py
+++ constants.py
@@ -1,7 +1,8 @@
 FILEPATH = r"./StatesDB.txt"
 LEARNINGMODE = True #Main operating mode
 STRONGMODE = False #Every sentence taught is repeated until it is first result in score for given input
-HITSCONST = 4  # how many words match
+DEBUG = True
+HITSCONST = 4  # Factor in scoring algo, how many points each hit worths
 RELATIONCONST = 3  # connection exists between nodes
 SCORECONST = 1  # adding up matching words weight
-DEBUG = True
+MAXENHANCMENTITER = 3 # maximum amount of iterations to improve a sentence
